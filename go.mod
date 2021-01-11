32ad382ba632716cf8f49e17024cb2a1870caa39
killroad <killroad@gmail.com>
2021-01-11 16:56:34 +0900

    프로젝트 초기화

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/sukmin/gople
 
-go 1.12
+go 1.15
--- main.go
+++ main.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"strconv"
-)
-
-const PORT  = 8080
-
 func main() {
-	portAddress := ":" + strconv.Itoa(PORT)
-
-	http.HandleFunc("/", func (w http.ResponseWriter,r *http.Request) {
-		fmt.Fprintln(w, "<!DOCTYPE html>")
-		fmt.Fprintln(w, "<html lang=\"ko\">")
-		fmt.Fprintln(w, "<head>")
-		fmt.Fprintln(w, "<meta charset=\"utf-8\">")
-		fmt.Fprintln(w, "<title>권석민의 블로그</title>")
-		fmt.Fprintln(w, "</head>")
-		fmt.Fprintln(w, "<body>")
-		fmt.Fprintln(w, "hello world")
-		fmt.Fprintln(w, "</body>")
-		fmt.Fprint(w, "</html>")
-	})
-
-	http.ListenAndServe(portAddress,nil)
+	print("hello")
 }
