5bbe729a4dd479804437bfab965b6561d07a75e9
killroad <killroad@gmail.com>
2021-11-23 02:14:41 +0900

    파피콘 추가, 스태틱 리소스 설정 추가

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	print("hello")
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, req *http.Request) {
+		http.ServeFile(w, req, "./static/favicon.ico")
+	})
+
+	log.Println("Listening on :8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
